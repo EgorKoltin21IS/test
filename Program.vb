cc8f6e0d10021740dde742db375f14d02e55d113
EgorKoltin21IS <123120523+EgorKoltin21IS@users.noreply.github.com>
2023-01-20 00:11:55 +0300

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -3,7 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.Write("Hello world!")
-        Console.Write("”гадай число")
         Console.Read()
     End Sub
 End Module
