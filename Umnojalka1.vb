4d949cc3344655fed6a5bb2c117cb80d800679d3
EgorKoltin21IS <123120523+EgorKoltin21IS@users.noreply.github.com>
2023-01-23 09:22:21 +0300

    Add files via upload

---
+++ Umnojalka1.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim a As Int32, b As Int32, c As Int32
+        c = 2147483630
+        a = 1
+        Console.WriteLine("Привет, я перемножаю числа. Введи числа чтобы перемножить, 1 чтобы прекратить и вывести результат")
+        Do Until b = 1
+
+            b = Console.ReadLine()
+            If a < (c / b) Then
+                a = a * b
+            Else
+                Console.WriteLine("Ошибка, память переполнена")
+                Exit Do
+            End If
+        Loop
+        Console.Write("Получилось: ")
+        Console.Write(a)
+    End Sub
+End Module
