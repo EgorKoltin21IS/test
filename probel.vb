ed3cec647ea9e79b3823a588ccf2a455593b45cc
EgorKoltin21IS <123120523+EgorKoltin21IS@users.noreply.github.com>
2023-01-23 10:09:42 +0300

    Add files via upload

---
+++ probel.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim s As String
+        Dim counter As Integer = 0
+        Console.WriteLine("Введите строку")
+        s = Console.ReadLine()
+        For i = 0 To s.Length - 1
+            Console.Write(s.Chars(i))
+            Console.WriteLine(Asc(s.Chars(i)))
+            If Asc(s.Chars(i)) = 32 Then
+                counter = counter + 1
+            End If
+        Next
+        Console.WriteLine("В строке {0} пробелов", counter)
+    End Sub
+End Module
