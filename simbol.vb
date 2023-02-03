c855b5fe1bb53de1e26672c25c2f389758d6eaa0
EgorKoltin21IS <123120523+EgorKoltin21IS@users.noreply.github.com>
2023-02-03 08:19:35 +0300

    Add files via upload

--- simbol.vb
+++ simbol.vb
@@ -4,7 +4,7 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("введите строку")
         Dim s As String
-        Dim counter As Integer, sym As Integer, allsym As Integer, number As Integer = 0
+        Dim counter As Integer, sym As Integer, allsym As Integer, gl As Integer, sgl As Integer, number As Integer = 0
         s = Console.ReadLine()
         For i As Integer = 0 To s.Length - 1
             Console.Write(s.Chars(i))
@@ -13,12 +13,18 @@ Module Program
             Select Case Asc(s.Chars(i))
                 Case 32
                     counter = counter + 1
-                Case 0 To 126
+                Case 97 To 122
+                    gl = gl + 1
+                Case 65 To 90
+                    sgl = sgl + 1
+                Case 48 To 57
                     allsym = allsym + 1
             End Select
 
         Next
         Console.WriteLine("В строке {0} пробелов", counter)
-        Console.WriteLine("В строке {0} символов", allsym)
+        Console.WriteLine("В строке {0} гласных", gl)
+        Console.WriteLine("В строке {0} согласных", sgl)
+        Console.WriteLine("В строке {0} знаков препинания", allsym)
     End Sub
 End Module
