6a8f939184a28d2d6ff8d55c9de015cf28d01b6b
EgorKoltin21IS <123120523+EgorKoltin21IS@users.noreply.github.com>
2023-03-03 02:08:39 +0300

    Add files via upload

---
+++ new/time.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+
+Module Program
+    Dim sec As Integer, Minute As Integer, hours As Integer
+    Sub Main(args As String())
+        Console.WriteLine("Ведите время в секундах")
+        sec = Console.ReadLine()
+        hours = sec / 3600
+        Minute = (sec Mod 3600) / 60
+        sec = sec Mod 60
+        Console.Write("{0} часов", hours)
+        Console.Write(" {0} минут", Minute)
+        Console.Write(" {0} секунд", sec)
+    End Sub
+End Module
