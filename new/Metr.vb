8dfffc1a431260b22e6ca68b57d1f816adcd1a32
EgorKoltin21IS <123120523+EgorKoltin21IS@users.noreply.github.com>
2023-03-18 15:36:32 +0300

    Add files via upload

---
+++ new/Metr.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim sm As Integer, dm As Integer, metr As Integer
+        Console.WriteLine("Длина в сантиметрах")
+        sm = Console.ReadLine
+        metr = sm \ 100
+        dm = sm \ 10
+        sm = sm Mod 100
+        Console.Write("{0}м {1}см", metr, sm)
+        sm = sm Mod 10
+        Console.Write("{0}дм {1}см", dm, sm)
+        dm = dm Mod 10
+        Console.Write("{0}м {1}дм {2}см", metr, dm, sm)
+    End Sub
+End Module
